9d18ecae3c72e61ff0224b0c96eeaba83a0a83fd
mriduldutt <mirdul12dutt@gmail.com>
2024-02-08 13:51:57 +0530

    saveit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 import { useState } from "react";
 import Alert from "./Alert";
-// import Aboutus from "./Components/Aboutus";
+import About from "./Components/About";
+import {
+  BrowserRouter as Router,Route,Routes,Link
+} from "react-router-dom";
 
 function App() {
   // JSX
@@ -34,31 +37,36 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been enabled", "success");
-    
+      document.title = 'Text Utils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'whitesmoke';
       showAlert("Light Mode has been enabled", "success");
+      document.title = 'Text Utils - Light Mode';
     }
   }
 
 
   return (
     <>
+      <Router>
       {/* {<Navbar/>} */}
       {/* {<Navbar title="TextUtils" aboutText="About us"/>} */}
       <Navbar title="TextUtils" aboutText="About us" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-      <TextForm  showAlert ={showAlert} heading="Enter the text to analysis below" mode={mode}/>
-     
+      <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" 
+          element={<TextForm  showAlert ={showAlert} heading="Enter the text to analysis below" mode={mode}/>}/>
+        </Routes>
+
       {/* <Aboutus/> */}
       </div>
 
-     
-     
       <img src={logo} alt=""/>
+      </Router>
     </>
   );
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 import React, { useState }  from "react";
 
-export default function Aboutus() {
+export default function About() {
 const [mystyle,setMystyle] = useState({
     color: "black",
     backgroundColor: "white",
